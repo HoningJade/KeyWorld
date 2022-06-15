e151ed96f0aecc71a9cee91070d253d5a4a71538
Ray <schrodinger@sjtu.edu.cn>
2022-06-15 02:42:08 -0400

    kotlin project create

---
+++ front-end/app/src/main/java/cn/edu/sjtu/keyworldteam/keyworld/MainActivity.kt
@@ -0,0 +1,11 @@
+package cn.edu.sjtu.keyworldteam.keyworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
