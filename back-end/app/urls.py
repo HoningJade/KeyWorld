19d619fbb472779fd31a5e91ff5e52de3bed0473
ChurongJi <churong@umich.edu>
2022-07-11 22:55:05 +0000

    Key upload backend updated

--- back-end/app/urls.py
+++ back-end/app/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('keyUpload/', views.keyUpload, name='keyUpload'),
     path('residentUpdate/', views.residentUpdate, name='residentUpdate'),
+    path('serviceList/', views.serviceList, name='serviceList'),
+    path('liveChat/', views.liveChat, name='liveChat'),
+    # path(r'^keyUpload/$', views.keyUpload, name='keyUpload'),
 ]
+
+
--- back-end/app/views.py
+++ back-end/app/views.py
@@ -1,11 +1,49 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+import json
+from django.db import connection
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
 
 def keyUpload(request):
+    if request.method == 'POST':
+        if request.POST.get('room') and request.POST.get('key'):
+            room_number = request.POST.get('room')
+            key = request.POST.get('key')
+            cursor = connection.cursor()
+            cursor.execute('INSERT INTO rooms (room_number, key, availability) VALUES '
+                        '(%s, %s, 0);', (room_number, key))
+            context = {
+                'aaa': '1234',
+                'room_number': room_number,
+                'nfc_key': key
+            }
+            return render(request, 'keyUpload.html', {})
+        return render(request, 'error.html', {})
     return render(request, 'keyUpload.html', {})
 
 def residentUpdate(request):
     return render(request, 'residentUpdate.html', {})
+
+def serviceList(request):
+    return render(request, 'serviceList.html', {})
+
+def liveChat(request):
+    return render(request, 'liveChat.html', {})
+
+
+# @csrf_exempt
+# def UploadRoomKey(request):
+#     if request.method != 'POST':
+#         return HttpResponse(status=404)
+#     form = InputForm(request.POST)
+#     aaa = '2333'
+#     room_num = form['room_number']
+#     nfc_key = form['nfc_key']
+#     cursor = connection.cursor()
+#     cursor.execute('INSERT INTO rooms (room_number, key, availability) VALUES '
+#                    '(%s, %s, 0);', (room_num, nfc_key))
+#     return render(request, 'keyUpload.html', {})
+#     return render(request, 'test.html', {})
