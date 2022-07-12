4ad042d754666b1919c5e8abe44680ffe71e48e1
cinryzh <cinryzh@gmail.com>
2022-07-12 00:04:33 -0400

    test for service list url

--- back-end/app/urls.py
+++ back-end/app/urls.py
@@ -5,9 +5,9 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('keyUpload/', views.keyUpload, name='keyUpload'),
     path('residentUpdate/', views.residentUpdate, name='residentUpdate'),
-    path('serviceList/', views.serviceList, name='serviceList'),
+    path('serviceList/', views.serviceRequestList, name='serviceList'),
     path('liveChat/', views.liveChat, name='liveChat'),
     # path(r'^keyUpload/$', views.keyUpload, name='keyUpload'),
-    path('serviceRequestList/', views.serviceRequestList, name='serviceRequestList'),
+    # path('serviceRequestList/', views.serviceRequestList, name='serviceRequestList'),
     path('serviceSelect/', views.serviceSelect, name='serviceSelect'),
 ]
