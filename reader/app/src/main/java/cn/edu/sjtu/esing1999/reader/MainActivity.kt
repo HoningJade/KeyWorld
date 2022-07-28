0f321036549d1b07858d8f04fc18c5fe328dd2ec
esing <esing@umich.edu>
2022-07-28 17:23:29 -0400

    Card emulator and reader implemented

---
+++ reader/app/src/main/java/cn/edu/sjtu/esing1999/reader/MainActivity.kt
@@ -0,0 +1,42 @@
+package cn.edu.sjtu.esing1999.reader
+
+import android.nfc.NfcAdapter
+import android.nfc.Tag
+import android.nfc.tech.IsoDep
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), NfcAdapter.ReaderCallback {
+
+    private var nfcAdapter: NfcAdapter? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+    }
+
+    public override fun onResume() {
+        super.onResume()
+        nfcAdapter?.enableReaderMode(this, this,
+            NfcAdapter.FLAG_READER_NFC_A or
+                    NfcAdapter.FLAG_READER_SKIP_NDEF_CHECK,
+            null)
+    }
+
+    public override fun onPause() {
+        super.onPause()
+        nfcAdapter?.disableReaderMode(this)
+    }
+
+    override fun onTagDiscovered(tag: Tag?) {
+        val isoDep = IsoDep.get(tag)
+        isoDep.connect()
+        val response = isoDep.transceive(Utils.hexStringToByteArray(
+            "00A4040007A0000002471001"))
+        runOnUiThread { textView.append("\nDoor Unlocked!") }
+        isoDep.close()
+    }
+}
