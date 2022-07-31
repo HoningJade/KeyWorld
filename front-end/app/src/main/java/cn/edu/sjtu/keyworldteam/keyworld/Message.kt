1c7c2dbc43b3b8d6f3cdebd64f83db431f0e6f2e
Ray <schrodinger@sjtu.edu.cn>
2022-07-31 15:51:32 -0400

    Remove unneeded timestamp

--- front-end/app/src/main/java/cn/edu/sjtu/keyworldteam/keyworld/Message.kt
+++ front-end/app/src/main/java/cn/edu/sjtu/keyworldteam/keyworld/Message.kt
@@ -3,11 +3,11 @@ package cn.edu.sjtu.keyworldteam.keyworld
 sealed class Message {
     data class Hotel(
         val text : String,
-        val time : String
+//        val time : String
     ) : Message()
 
     data class User(
         val text : String,
-        val time : String
+//        val time : String
     ) : Message()
 }
